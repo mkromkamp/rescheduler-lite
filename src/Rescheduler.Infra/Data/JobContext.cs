a1d4b3db5fa7c38dcfa77df2ac49335ba55f60d6
Martin Kromkamp <m.kromkamp@gmail.com>
2021-01-09 19:59:51 +0000

    Fixed sqlite issues

--- src/Rescheduler.Infra/Data/JobContext.cs
+++ src/Rescheduler.Infra/Data/JobContext.cs
@@ -7,9 +7,6 @@ namespace Rescheduler.Infra.Data
 {
     public class JobContext : DbContext
     {
-        public JobContext() : base()
-        { }
-
         public JobContext(DbContextOptions<JobContext> options)
             : base(options)
         { }
@@ -19,7 +16,7 @@ namespace Rescheduler.Infra.Data
         public DbSet<ScheduledJob> ScheduledJobs { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite("Filename=:memory:") // Data Source=rescheduler.db
+            => options.UseSqlite("Filename=:memory:?cache=shared") // Data Source=rescheduler.db
             .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
