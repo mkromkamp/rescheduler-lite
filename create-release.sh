ba263e53ee1a46e5fa459e6294088910651faee0
Martin Kromkamp <m.kromkamp@gmail.com>
2021-01-24 10:22:05 +0000

    Fix create release

--- create-release.sh
+++ create-release.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 
-dotnet publish -c Release -o ../../bin/ -r linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:PublishReadyToRun=true --self-contained true
+dotnet publish -c Release --project ./src/Rescheduler.Api -o ../../bin/ -r linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:PublishReadyToRun=true --self-contained true
