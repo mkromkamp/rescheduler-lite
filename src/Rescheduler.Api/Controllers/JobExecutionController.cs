17a4030a6f6b556f99ddd8d3045567bd6c1a7cbc
Martin Kromkamp <m.kromkamp@gmail.com>
2021-01-31 14:59:17 +0000

    Get all job executions with filter

--- src/Rescheduler.Api/Controllers/JobExecutionController.cs
+++ src/Rescheduler.Api/Controllers/JobExecutionController.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Rescheduler.Api.Models;
+using Rescheduler.Core.Entities;
 using Rescheduler.Core.Handlers;
 
 namespace Rescheduler.Api.Controllers
@@ -20,9 +22,20 @@ namespace Rescheduler.Api.Controllers
         }
 
         [HttpGet("jobs/{jobId}/executions")]
-        public async Task<IActionResult> GetAllAsync(Guid jobId, CancellationToken ctx)
+        public async Task<IActionResult> GetByJobIdAsync(Guid jobId, CancellationToken ctx)
         {
             var result = await _mediator.Send(new GetJobExecutionsRequest(jobId), ctx);
+
+            if(result.Executions.Any())
+                return Ok(result.Executions.Select(j => JobExecutionResponse.From(j)).ToList());
+            
+            return NotFound();
+        }
+
+        [HttpGet("jobs/executions")]
+        public async Task<IActionResult> GetAllAsync([FromQuery] IEnumerable<ExecutionStatus> statuses, [FromQuery] string? subject, CancellationToken ctx, [FromQuery] int top = 20, [FromQuery] int skip = 0)
+        {
+            var result = await _mediator.Send(new GetExecutionsRequest(statuses, subject, top, skip), ctx);
 
             if(result.Executions.Any())
                 return Ok(result.Executions.Select(j => JobExecutionResponse.From(j)).ToList());
---
+++ src/Rescheduler.Core/Handlers/GetExecutionsHandler.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Rescheduler.Core.Entities;
+using Rescheduler.Core.Interfaces;
+
+namespace Rescheduler.Core.Handlers
+{
+    public class GetExecutionsHandler : IRequestHandler<GetExecutionsRequest, GetExecutionsResponse>
+    {
+        private readonly IRepository<JobExecution> _jobExecutionRepository;
+
+        public GetExecutionsHandler(IRepository<JobExecution> jobExecutionRepository)
+        {
+            _jobExecutionRepository = jobExecutionRepository;
+        }
+
+        public async Task<GetExecutionsResponse> Handle(GetExecutionsRequest request, CancellationToken cancellationToken)
+        {
+            var jobExecutions = await _jobExecutionRepository.GetManyAsync(q =>
+                q.Where(e =>
+                    !request.Statuses.Any() || request.Statuses.Contains(e.Status)
+                )
+                .Where(e => request.Subject == null || e.Job.Subject == request.Subject)
+                .OrderByDescending(e => e.ScheduledAt)
+                .Skip(request.Skip)
+                .Take(request.Top),
+                cancellationToken);
+
+            return new GetExecutionsResponse(jobExecutions);
+        }
+    }
+
+    public record GetExecutionsRequest(IEnumerable<ExecutionStatus> Statuses, string? Subject, int Top, int Skip) : IRequest<GetExecutionsResponse>;
+
+    public record GetExecutionsResponse(IEnumerable<JobExecution> Executions);
+}
