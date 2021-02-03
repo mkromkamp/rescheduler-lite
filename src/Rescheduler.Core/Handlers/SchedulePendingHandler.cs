b05dd58a69a861881b99cb2e48dfa6a74fe9569d
Martin Kromkamp <m.kromkamp@gmail.com>
2021-02-03 19:53:54 +0000

    Slight cleanup

--- src/Rescheduler.Core/Handlers/SchedulePendingHandler.cs
+++ src/Rescheduler.Core/Handlers/SchedulePendingHandler.cs
@@ -24,26 +24,29 @@ namespace Rescheduler.Core.Handlers
         public async Task<SchedulePendingResponse> Handle(SchedulePendingRequest request, CancellationToken cancellationToken)
         {
             var pendingJobs = await _jobExecutionRepository.GetAndMarkPending(1000, DateTime.UtcNow.AddSeconds(10), cancellationToken);
+            pendingJobs = pendingJobs.ToList();
             var result = new SchedulePendingResponse(pendingJobs.Count());
 
             if (pendingJobs.Any())
             {
-                var now = DateTime.UtcNow;
-                pendingJobs.ToList().ForEach(j => j.Queued(now));
-                await _JobExecutionRepo.UpdateManyAsync(pendingJobs, cancellationToken);
-
-                foreach (var pending in pendingJobs)
+                // Try to queue scheduled jobs
+                if(!await _jobPublisher.PublishManyAsync(pendingJobs.Select(p => p.Job), cancellationToken))
                 {
-                    if (await _jobPublisher.PublishAsync(pending.Job, cancellationToken))
-                    {
-                        pending.Queued(DateTime.UtcNow);
-                    }
+                    // Failed to publish, reschedule
+                    pendingJobs.ToList().ForEach(p => p.Scheduled());
+                    await _JobExecutionRepo.UpdateManyAsync(pendingJobs, cancellationToken);
+                    
+                    return new SchedulePendingResponse(0);
                 }
 
+                // Mark messages as queued
+                var now = DateTime.UtcNow;
+                pendingJobs.ToList().ForEach(p => p.Queued(now));
                 await _JobExecutionRepo.UpdateManyAsync(pendingJobs, cancellationToken);
 
                 foreach (var pending in pendingJobs)
                 {
+                    // If there is a next schedule available for the job queue it
                     if (pending.Job.TryGetNextRun(DateTime.UtcNow, out var nextRun) && nextRun.HasValue)
                     {
                         var nextJobExecution = JobExecution.New(pending.Job, nextRun.Value);
