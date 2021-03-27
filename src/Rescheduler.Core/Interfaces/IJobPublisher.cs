801669ab5cbc724c2f37ce816eac90c84c71056f
Martin Kromkamp <m.kromkamp@gmail.com>
2021-03-27 08:43:08 +0000

    Docs added

--- src/Rescheduler.Core/Interfaces/IJobPublisher.cs
+++ src/Rescheduler.Core/Interfaces/IJobPublisher.cs
@@ -7,8 +7,20 @@ namespace Rescheduler.Core.Interfaces
 {
     public interface IJobPublisher
     {
-         Task<bool> PublishAsync(Job job, CancellationToken ctx);
+        /// <summary>
+        /// Publish a <see cref="Job"/>.
+        /// </summary>
+        /// <param name="job">The job</param>
+        /// <param name="ctx">The CancellationToken</param>
+        /// <returns>A <see cref="Task{T}"/> representing the async operation</returns>
+        Task<bool> PublishAsync(Job job, CancellationToken ctx);
 
-         Task<bool> PublishManyAsync(IEnumerable<Job> jobs, CancellationToken ctx);
+        /// <summary>
+        /// Batch publish a list of <see cref="Job"/>.
+        /// </summary>
+        /// <param name="jobs">The jobs</param>
+        /// <param name="ctx">The CancellationToken</param>
+        /// <returns>A <see cref="Task{T}"/> representing the async operation</returns>
+        Task<bool> PublishManyAsync(IEnumerable<Job> jobs, CancellationToken ctx);
     }
 }
--- src/Rescheduler.Infra/Messaging/RabbitMqOptions.cs
+++ src/Rescheduler.Infra/Messaging/RabbitMqOptions.cs
@@ -2,6 +2,9 @@ namespace Rescheduler.Infra.Messaging
 {
     internal class RabbitMqOptions
     {
+        /// <summary>
+        /// The name of the exchange that jobs are published to
+        /// </summary>
         public string JobsExchange { get; set; } = "jobs";
     }
 }
