7fd8b06b92b00739c00cf89df63dc4c620f12539
Martin Kromkamp <m.kromkamp@gmail.com>
2024-08-04 08:20:19 +0100

    fix; API cron validation (#38)

---
+++ src/Rescheduler.Api/Models/ValidateCronAttribute.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using Cronos;
+
+namespace Rescheduler.Api.Models;
+
+public class ValidateCronAttribute : ValidationAttribute
+{
+    private static string GetErrorMessage(string? cron) =>
+        $"Invalid cron expression {cron}";
+
+    protected override ValidationResult? IsValid(
+        object? value, ValidationContext validationContext)
+    {
+        var cron = (string?)value;
+        
+        if (String.IsNullOrWhiteSpace(cron) || !CronExpression.TryParse(cron, out _))
+        {
+            return new ValidationResult(GetErrorMessage(cron));
+        }
+        
+        return ValidationResult.Success;
+    }
+}
